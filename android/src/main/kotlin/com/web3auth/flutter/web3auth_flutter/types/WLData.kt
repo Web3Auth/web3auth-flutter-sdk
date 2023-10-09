b1c73fd930a232f08be85a8915765a555470e280
Gaurav Goel <grvgoel19@gmail.com>
2023-10-09 08:57:13 +0530

    bump web3auth android sdk version and code refactored
    Signed-off-by: Gaurav Goel <gaurav@tor.us>

--- android/src/main/kotlin/com/web3auth/flutter/web3auth_flutter/types/WLData.kt
+++ android/src/main/kotlin/com/web3auth/flutter/web3auth_flutter/types/WLData.kt
@@ -1,23 +1,29 @@
 package com.web3auth.flutter.web3auth_flutter.types
 
+import com.web3auth.core.types.Language
+import com.web3auth.core.types.ThemeModes
 import com.web3auth.core.types.WhiteLabelData
 
-data class WLData (
-    private var name : String? = null,
-    private var logoLight : String? = null,
-    private var logoDark : String? = null,
-    private var defaultLanguage : String? = "en",
-    private var dark : Boolean? = false,
-    private var theme : HashMap<String, String>? = null
+data class WLData(
+    private var appName: String? = null,
+    private var appUrl: String? = null,
+    private var logoLight: String? = null,
+    private var logoDark: String? = null,
+    private var defaultLanguage: Language? = Language.EN,
+    private var mode: ThemeModes? = ThemeModes.LIGHT,
+    private var theme: HashMap<String, String>? = null,
+    private var useLogoLoader: Boolean? = false
 ) {
     fun toWhiteLabelData() : WhiteLabelData {
         return WhiteLabelData(
-            name = name,
+            appName = appName,
+            appUrl = appUrl,
             logoLight = logoLight,
             logoDark = logoDark,
             defaultLanguage = defaultLanguage,
-            dark = dark,
-            theme = theme
+            mode = mode,
+            theme = theme,
+            useLogoLoader = useLogoLoader
         )
     }
 }
