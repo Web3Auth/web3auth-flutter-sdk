bfd3718a0d62d45a17254b4336d0f132f55e4c69
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2024-03-07 10:25:41 +0530

    add interface documentation

--- lib/input.dart
+++ lib/input.dart
@@ -1,14 +1,36 @@
 import 'dart:collection';
 
 import 'package:web3auth_flutter/enums.dart';
+import 'package:web3auth_flutter/web3auth_flutter.dart';
 
 class LoginParams {
+  /// [loginProvider] sets the oAuth login method to be used. You can use any of the
+  /// valid [Provider] from the supported list.
   final Provider loginProvider;
+
   final String? dappShare;
+
+  /// [curve] will be used to determine the public key encoded in the jwt token which returned in
+  /// `getUserInfo` function after user login.
+  ///
+  /// This parameter won't change format of private key returned by We3Auth. Private key returned
+  /// by [Web3AuthFlutter.getPrivKey] is always secp256k1. To get the ed25519 key
+  /// you can use [Web3AuthFlutter.getEd25519PrivKey] method.
+  ///
+  /// The default value is [Curve.secp256k1].
   final Curve? curve;
+
+  /// [extraLoginOptions] can be used to set the OAuth login options for corresponding [loginProvider].
+  ///
+  /// For instance, you'll need to pass user's email address as `login_hint` for [Provider.email_passwordless].
   final ExtraLoginOptions? extraLoginOptions;
+
+  /// Deeplinking for the application where user will be redirected after login.
   final Uri? redirectUrl;
+
   final String? appState;
+
+  /// Customize the MFA screen shown to the user during OAuth authentication.
   final MFALevel? mfaLevel;
 
   LoginParams({
@@ -33,18 +55,44 @@ class LoginParams {
 }
 
 class LoginConfigItem {
+  /// Custom verifier name given in the developer dashboard.
   final String verifier;
+
+  /// The type of login for custom verifier.
   final TypeOfLogin typeOfLogin;
+
+  /// Client id provided by your login provider used for custom verifier.
   final String clientId;
+
+  /// Display name for the verifier. If null, the default name is used.
   final String? name;
+
+  /// Description for the button. If provided, it renders as a full length button. else, icon button.
   final String? description;
+
+  /// The field in JWT token which maps to verifier id. Please make sure you selected
+  /// correct JWT verifier id in the developer dashboard.
   final String? verifierSubIdentifier;
+
+  /// Logo to be shown on mouse hover.
   final String? logoHover;
+
+  /// Light logo for the dark theme.
   final String? logoLight;
+
+  /// Dark logo for the light theme.
   final String? logoDark;
+
+  /// Show login button on the main list.
   final bool? mainOption;
+
+  /// Whether to show the login button on Modal.
   final bool? showOnModal;
+
+  /// Whether to show the login button on the Desktop.
   final bool? showOnDesktop;
+
+  /// Whether to show the login button on Mobile.
   final bool? showOnMobile;
 
   LoginConfigItem({
@@ -84,18 +132,49 @@ class LoginConfigItem {
 
 class ExtraLoginOptions {
   final Map? additionalParams;
+
+  /// Your custom authentication domain. For example, if you are using Auth0,
+  /// it can be example.au.auth0.com.
   final String? domain;
+
+  /// Client id provided by your login provider used for custom verifier
   final String? client_id;
+
+  /// The value used to account for clock skew in JWT expirations. The value is
+  /// in the seconds, and ideally should no more than 60 seconds or 120 seconds at max.
   final String? leeway;
+
+  /// The field in JWT token which maps to verifier id. Please make sure you selected
+  /// correct JWT verifier id in the developer dashboard.
   final String? verifierIdField;
+
+  /// Whether the verifier id field is case sensitive or not.
   final bool? isVerifierIdCaseSensitive;
+
+  /// Allows developers the configure the display of UI. Checkout [Display] for more
+  /// details.
   final Display? display;
+
+  /// Prompt shown to the user during authentication process. Checkout [Prompt] for more
+  /// details.
   final Prompt? prompt;
+
+  /// Max time allowed without reauthentication. If the last time user authenticated
+  /// is greater than this value, then user must reauthenticate.
   final String? max_age;
+
+  /// The space separated list of language tags, ordered by preference. For instance
+  /// `fr-CA fr en`.
   final String? ui_locales;
+
+  /// [id_token_hint] denoates the previously issued ID token.
   final String? id_token_hint;
+
   final String? id_token;
+
+  /// [login_hint] is used to send the user's email address during [Provider.email_passwordless].
   final String? login_hint;
+
   final String? acr_values;
   final String? scope;
   final String? audience;
@@ -103,6 +182,10 @@ class ExtraLoginOptions {
   final String? state;
   final String? response_type;
   final String? nonce;
+
+  /// [redirectUrl] can be used to specify the default url, where your custom jwt verifier can redirect
+  /// your browser to with the result. If you are using Auth0, it must be whitelisted in the Allowed
+  /// Callback URLs in your Auth0's application.
   final String? redirect_uri;
 
   ExtraLoginOptions({
@@ -155,13 +238,35 @@ class ExtraLoginOptions {
 }
 
 class WhiteLabelData {
+  /// Display name for the app in the UI.
   final String? appName;
+
+  /// App logo to be used in dark mode.
   final String? logoLight;
+
+  /// App logo to be used in light mode.
   final String? logoDark;
+
+  /// Language which will be used by Web3Auth, app will use browser language if not specified.
+  ///
+  /// Default language is [Language.en]. Checkout [Language] for supported languages.
   final Language? defaultLanguage;
+
+  /// Theme mode for the login modal
+  ///
+  /// Default value is [ThemeModes.auto].
   final ThemeModes? mode;
+
+  /// Used to customize the theme of the login modal.
   final HashMap? theme;
+
+  /// Url to be used in the Modal
   final String? appUrl;
+
+  /// Use logo loader.
+  ///
+  /// If [logoDark] and [logoLight] are null, the default Web3Auth logo
+  /// will be used for the loader.
   final bool? useLogoLoader;
 
   WhiteLabelData({
@@ -190,8 +295,14 @@ class WhiteLabelData {
 }
 
 class MfaSetting {
+  /// Whether the factor is enabled or not.
   final bool enable;
+
+  /// Priority of the factor over other factors.
   final int? priority;
+
+  /// Whether the factor is mandaotry or not for prove
+  /// the owenership.
   final bool? mandatory;
 
   MfaSetting({required this.enable, this.priority, this.mandatory});
@@ -202,16 +313,24 @@ class MfaSetting {
 }
 
 class MfaSettings {
+  /// Define the settings for device share factor.
   final MfaSetting? deviceShareFactor;
+
+  /// Define the settings for backup share factor.
   final MfaSetting? backUpShareFactor;
+
+  /// Define the settings for social backup factor.
   final MfaSetting? socialBackupFactor;
+
+  /// Define the settings for password factor.
   final MfaSetting? passwordFactor;
 
-  MfaSettings(
-      {this.deviceShareFactor,
-      this.backUpShareFactor,
-      this.socialBackupFactor,
-      this.passwordFactor});
+  MfaSettings({
+    this.deviceShareFactor,
+    this.backUpShareFactor,
+    this.socialBackupFactor,
+    this.passwordFactor,
+  });
 
   Map<String, dynamic> toJson() {
     return {
@@ -224,16 +343,51 @@ class MfaSettings {
 }
 
 class Web3AuthOptions {
+  /// Client id for web3auth.
+  ///
+  /// You can obtain your client id from the web3auth [developer dashboard](https://dashboard.web3auth.io/).
   final String clientId;
+
+  /// Web3Auth Network to use for the session & the issued idToken.
+  ///
+  /// User [Network.sapphire_mainnet] for production build.
   final Network network;
+
+  ///
   final BuildEnv? buildEnv;
+
+  /// Define the desired Web3Auth service url.
   final String? sdkUrl;
+
+  /// Deeplinking for the application where user will be redirected after login.
+  /// Ideally, it should be bundleId and package name for iOS and Android respectively.
+  ///
+  /// While using redirectUrl, please make sure you have whitelisted it
+  /// developer dashboard. Checkout [SDK reference](https://web3auth.io/docs/sdk/pnp/flutter/install#configuration-1) more details.
   final Uri redirectUrl;
+
+  /// WhiteLabel options for web3auth. It helps you define
+  /// custom UI, branding, and translations for your brand app.
+  ///
+  /// Checkout [WhiteLabelData] for more details.
   final WhiteLabelData? whiteLabel;
+
+  /// Login config for the custom verifiers.
   final HashMap<String, LoginConfigItem>? loginConfig;
+
+  /// Use [useCoreKitKey] to get the core kit key.
   final bool? useCoreKitKey;
+
   final ChainNamespace? chainNamespace;
+
+  /// Allows developers to configure the [MfaSettings] for authentication.
+  ///
+  /// Checkout [MFA SDK Reference](https://web3auth.io/docs/sdk/pnp/flutter/mfa) for more details.
   final MfaSettings? mfaSettings;
+
+  /// [sessionTime] allows developers to configure the session management time.
+  ///
+  /// Session Time is in seconds, default is 86400 seconds which is 1 day. [sessionTime] can be max 7 days.
   final int? sessionTime;
 
   Web3AuthOptions({
