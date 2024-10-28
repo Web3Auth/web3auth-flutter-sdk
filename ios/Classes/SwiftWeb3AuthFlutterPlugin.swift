6ecda2ccd5f79f5e606e5f2811958fdb2da2a846
Gaurav Goel <grvgoel19@gmail.com>
2024-10-28 17:20:11 +0530

    feat: update request methid in android and SwiftWeb3AuthFlutterPlugin.swift
    Signed-off-by: Gaurav Goel <gaurav@tor.us>

--- ios/Classes/SwiftWeb3AuthFlutterPlugin.swift
+++ ios/Classes/SwiftWeb3AuthFlutterPlugin.swift
@@ -253,4 +253,71 @@ struct RequestJson: Codable {
     let method: String
     let requestParams: [String]
     let path: String?
+    let appState: String?
+
+    enum CodingKeys: String, CodingKey {
+        case chainConfig, method, requestParams, path, appState
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        chainConfig = try container.decode(ChainConfig.self, forKey: .chainConfig)
+        method = try container.decode(String.self, forKey: .method)
+        path = try container.decodeIfPresent(String.self, forKey: .path)
+        appState = try container.decodeIfPresent(String.self, forKey: .appState)
+
+        let paramsArray = try container.decode([[String: AnyCodable]].self, forKey: .requestParams)
+        requestParams = paramsArray.map { paramDict in
+            // Convert each dictionary to JSON string
+            let jsonData = try! JSONEncoder().encode(paramDict)
+            return String(data: jsonData, encoding: .utf8)!
+        }
+    }
 }
+
+struct AnyCodable: Codable {
+    let value: Any
+
+    init(_ value: Any) {
+        self.value = value
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if let intValue = try? container.decode(Int.self) {
+            value = intValue
+        } else if let doubleValue = try? container.decode(Double.self) {
+            value = doubleValue
+        } else if let stringValue = try? container.decode(String.self) {
+            value = stringValue
+        } else if let boolValue = try? container.decode(Bool.self) {
+            value = boolValue
+        } else if let dictionaryValue = try? container.decode([String: AnyCodable].self) {
+            value = dictionaryValue
+        } else if let arrayValue = try? container.decode([AnyCodable].self) {
+            value = arrayValue
+        } else {
+            throw DecodingError.dataCorruptedError(in: container, debugDescription: "Unsupported type")
+        }
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        if let intValue = value as? Int {
+            try container.encode(intValue)
+        } else if let doubleValue = value as? Double {
+            try container.encode(doubleValue)
+        } else if let stringValue = value as? String {
+            try container.encode(stringValue)
+        } else if let boolValue = value as? Bool {
+            try container.encode(boolValue)
+        } else if let dictionaryValue = value as? [String: AnyCodable] {
+            try container.encode(dictionaryValue)
+        } else if let arrayValue = value as? [AnyCodable] {
+            try container.encode(arrayValue)
+        } else {
+            throw EncodingError.invalidValue(value, EncodingError.Context(codingPath: encoder.codingPath, debugDescription: "Unsupported type"))
+        }
+    }
+}
+
