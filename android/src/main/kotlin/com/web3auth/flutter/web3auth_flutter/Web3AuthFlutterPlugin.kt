877075952cef6190b5518350816828fee1d56d73
Gaurav Goel <grvgoel19@gmail.com>
2025-11-10 18:29:16 +0700

    feat: update android sdk and revert analytics change
    Signed-off-by: Gaurav Goel <grvgoel19@gmail.com>

--- android/src/main/kotlin/com/web3auth/flutter/web3auth_flutter/Web3AuthFlutterPlugin.kt
+++ android/src/main/kotlin/com/web3auth/flutter/web3auth_flutter/Web3AuthFlutterPlugin.kt
@@ -99,8 +99,6 @@ class Web3AuthFlutterPlugin : FlutterPlugin, ActivityAware, MethodCallHandler,
                 val obj = JSONObject(initArgs)
                 if (obj.has("redirectUrl")) initParams.redirectUrl =
                     obj.get("redirectUrl").toString()
-                // set flutter analytics flag to true
-                initParams.setFlutterAnalytics(true, "7.0.0")
                 // Log.d(initParams.toString(), "#initParams")
                 web3auth = Web3Auth(
                     initParams, activity!!
